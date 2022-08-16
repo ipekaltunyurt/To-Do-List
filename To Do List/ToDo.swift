63d97e7d1cc34fed9f2c65d760a658eee9ae7019
Ipek Altunyurt <saltunyurt@yahoo.com.tr>
2022-08-16 15:37:05 +0300

    "Iteration 0 is done!"

---
+++ To Do List/ToDo.swift
@@ -0,0 +1,15 @@
+//
+//  ToDo.swift
+//  To Do List
+//
+//  Created by İpek Altunyurt on 16.08.2022.
+//
+
+import UIKit
+
+class ToDo {
+    var name = " "
+    var important = false
+    
+    
+}
